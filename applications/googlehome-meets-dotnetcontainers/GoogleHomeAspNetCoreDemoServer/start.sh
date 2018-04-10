f82af09856e5c533089834b118138e01e6ae2f08
GOOGLE\talarico <talarico@google.com>
2018-04-10 15:25:44 -0700

    Add Stackdriver Debugger support to the Google Home Demo support.
    Will require whitelisting. Contact talarico@ for details.

---
+++ applications/googlehome-meets-dotnetcontainers/GoogleHomeAspNetCoreDemoServer/start.sh
@@ -0,0 +1,7 @@
+﻿#!/bin/sh
+
+dotnet /app/GoogleHomeAspNetCoreDemoServer.dll &
+
+APP_PID=$(echo $!)
+
+/usr/share/dotnet-debugger/agent/Google.Cloud.Diagnostics.Debug --application-id=$APP_PID --debugger=/usr/share/dotnet-debugger/debugger/google_cloud_debugger --module=server --version=1.0
